cb3f647c740f59e865acf968410804d506eec159
Pasang Sherpa <pasang_sh@hotmail.com>
2014-08-30 23:25:50 -0700

    gulp integration

---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+var gulp = require('gulp'),
+    pkg = require('./package.json'),
+    plugins = require('gulp-load-plugins')(),
+    banner = [
+        '/*!',
+        ' * <%= config.name %> - <%= config.description %>',
+        ' * @version v<%= config.version %> - <%= config.today %>',
+        ' * @link <%= config.homepage %>',
+        ' * @author <%= config.author %>',
+        ' * @license <%= config.license %>',
+        ' */',
+        ''
+    ].join('\n'),
+    config = pkg;
+
+config.src = "src/";
+config.dist = "dist/";
+config.today = (new Date()).toDateString();
+
+// CSS
+gulp.task('css', function() {
+    gulp.src([config.src + 'less/main.less'])
+        .pipe(plugins.less())
+        .pipe(plugins.header(banner, {
+            config: config
+        }))
+        .pipe(gulp.dest(config.dist + 'css'))
+        .pipe(plugins.minifyCss())
+        .pipe(plugins.rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest(config.dist + 'css'))
+        .pipe(plugins.notify({
+            message: 'CSS task complete'
+        }));
+});
+
+// JavaScripts
+gulp.task('js', function() {
+    return gulp.src(config.src + 'js/**/*.js')
+        .pipe(plugins.jshint('.jshintrc'))
+        .pipe(plugins.jshint.reporter('default'))
+        .pipe(plugins.header(banner, {
+            config: config
+        }))
+        .pipe(gulp.dest(config.dist + 'js'))
+        .pipe(plugins.rename({
+            suffix: '.min'
+        }))
+        .pipe(plugins.header(banner, {
+            config: config
+        }))
+        .pipe(plugins.uglify())
+        .pipe(gulp.dest(config.dist + 'js'))
+        .pipe(plugins.notify({
+            message: 'JavaScripts task complete'
+        }));
+});
+
+// Images
+gulp.task('img', function() {
+    return gulp.src('src/img/**/*')
+        .pipe(plugins.cache(plugins.imagemin({
+            optimizationLevel: 3,
+            progressive: true,
+            interlaced: true
+        })))
+        .pipe(gulp.dest(config.dist + 'img'))
+        .pipe(plugins.notify({
+            message: 'Images task complete'
+        }));
+});
+
+// Clean
+gulp.task('clean', function(cb) {
+    plugins.clean([config.dist + 'css', config.dist + 'js', config.dist + 'img'], cb)
+});
+
+// Default task
+gulp.task('default', ['clean'], function() {
+    gulp.start('css', 'js', 'img');
+});
+
